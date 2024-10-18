3272cbdfb97d96bd938de466c8139d478844aa48
Samllo95 <ulloajose34@gmail.com>
2024-10-17 19:57:03 -0600

    feat(/semana4/ejercicio1):Add class Config

---
+++ src/main/java/semana4/ejercicio1/Config.java
@@ -0,0 +1,36 @@
+package semana4.ejercicio1;
+
+public class Config {
+    private static Config instancia;
+
+    private String configGeneral;
+    private int time;
+
+    private Config(){
+        this.configGeneral="Configuracion por defecto";
+        this.time=30;
+    }
+
+    public static Config getInstancia(){
+        if (instancia == null) {
+            instancia =new Config();
+        }
+        return instancia;
+    }
+
+    public String getConfigGeneral() {
+        return configGeneral;
+    }
+
+    public void setConfigGeneral(String configGeneral) {
+        this.configGeneral = configGeneral;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
